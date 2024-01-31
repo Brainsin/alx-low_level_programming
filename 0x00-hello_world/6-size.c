0d78ac1bc98822fcd8d71ac00c84f0a4dd16114d
Brainsin <imamogiri1234@gmail.com>
2024-01-31 10:49:22 +0100

    tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int=i;
+	float=f;
+	char=c;
+
+	printf("the size of int is ,\lu sizeof(unsigned long(i))\n");
+	printf("the size of float is,\lu sizeof(unsigned long (f))\n");
+	printf("the size of char is ,\lu sizeof(unsigned long (c))\n");
+	return(0);
+}
