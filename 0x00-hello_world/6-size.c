afbde4e898e6ca8d95c23906717b49061510f2b1
Brainsin <imamogiri1234@gmail.com>
2024-02-03 22:34:19 +0100

    tasking

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+/**
+ * main - printf
+ * @i: integer
+ * @f: float
+ * @c: character
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int=i;
 	float=f;
 	char=c;
 
-	printf("the size of int is ,\lu sizeof(unsigned long(i))\n");
-	printf("the size of float is,\lu sizeof(unsigned long (f))\n");
-	printf("the size of char is ,\lu sizeof(unsigned long (c))\n");
-	return(0);
+	printf("the size of int is: %lu.\n" ,(unsigned long)sizeof(i));
+	printf("the size of float is: %lu.\n" ,(unsigned long)sizeof(f));
+	printf("the size of char is: %lu.\n" ,(unsigned long)sizeof(c))\n);
+	return (0);
 }
