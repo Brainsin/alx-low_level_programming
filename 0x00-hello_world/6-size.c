9a35b96af703866f3c5bc8a69ea8593c5ff079b3
Brainsin <imamogiri1234@gmail.com>
2024-05-09 02:28:48 +0100

    stress

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry pont
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char is:%lu byte(s)\n",  sizeof(char));
+	printf("Size of an int is:%lu byte(s)\n", sizeof(int));
+	printf("Size of a long int is:%lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is:%lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%lu byte(s)\n", sizeof(float));
+	return (0);
+}
