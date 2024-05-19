e9f3f292bd705009ef8132fde5a822eca09ae9cc
Brainsin <imamogiri1234@gmail.com>
2024-05-19 02:29:13 +0100

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+
+	while (a <= z){
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
