d9b29589da6286087b5b8a117e8abb86d8e2dad9
Brainsin <imamogiri1234@gmail.com>
2024-07-25 03:57:30 +0100

    keep pushing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int i = 0;
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	while (str[i] != '\0')
 	{
 		putchar (str[i]);
 		i++;
 	}
 	putchar ('\n');
-	return (0);
+	return (1);
 }
