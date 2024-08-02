3981625b49b0467416704639f3198985249b1d44
Brainsin <imamogiri1234@gmail.com>
2024-08-02 23:28:12 +0100

    redebugging

--- 0x03_debugging/0-main.c
+++ 0x03_debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - returns the sign of any given integer
+ *
+ * Return: 0 always
+ */
 
 int main(void)
 {
